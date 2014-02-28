958421bddcb0afc17e82c451ea75dc02636dee4a
Adam Sheehan <a.t.sheehan@gmail.com>
2014-02-28 16:02:26 -0500

    Clean up a bit of pong js.

---
+++ public/js/key_codes.js
@@ -0,0 +1,6 @@
+var A_KEY = 65;
+var Q_KEY = 81;
+var P_KEY = 80;
+var L_KEY = 76;
+var UP_KEY = 38;
+var DOWN_KEY = 40;
--- public/js/pong.js
+++ public/js/pong.js
@@ -1,16 +1,8 @@
 var canvas = document.getElementById('canvas');
-var context = canvas.getContext('2d');
+var ctx = canvas.getContext('2d');
 
-var CANVAS_WIDTH = context.canvas.width;
-var CANVAS_HEIGHT = context.canvas.height;
-
-var A_KEY = 65;
-var Q_KEY = 81;
-var P_KEY = 80;
-var L_KEY = 76;
-
-var UP_ARROW = 38;
-var DOWN_ARROW = 40;
+var CANVAS_WIDTH = ctx.canvas.width;
+var CANVAS_HEIGHT = ctx.canvas.height;
 
 var PADDLE_WIDTH = CANVAS_WIDTH / 32;
 var PADDLE_HEIGHT = CANVAS_HEIGHT / 8;
@@ -18,14 +10,25 @@ var PADDLE_HEIGHT = CANVAS_HEIGHT / 8;
 var BALL_RADIUS = PADDLE_WIDTH / 2;
 
 var PADDLE_START_Y = (CANVAS_HEIGHT - PADDLE_HEIGHT) / 2;
-var PADDLE1_START_X = CANVAS_WIDTH / 10;
-var PADDLE2_START_X = (9 * CANVAS_WIDTH / 10) - PADDLE_WIDTH;
+var LEFT_PADDLE_START_X = CANVAS_WIDTH / 10;
+var RIGHT_PADDLE_START_X = (9 * CANVAS_WIDTH / 10) - PADDLE_WIDTH;
 
 var PADDLE_SPEED = 5;
 var BALL_SPEED = 3;
 
-var p1Paddle = { x: PADDLE1_START_X, y: PADDLE_START_Y };
-var p2Paddle = { x: PADDLE2_START_X, y: PADDLE_START_Y };
+var leftPaddle = {
+  x: LEFT_PADDLE_START_X,
+  y: PADDLE_START_Y,
+  movingUp: false,
+  movingDown: false
+};
+
+var rightPaddle = {
+  x: RIGHT_PADDLE_START_X,
+  y: PADDLE_START_Y,
+  movingUp: false,
+  movingDown: false
+};
 
 var ball = {
   x: CANVAS_WIDTH / 2,
@@ -34,176 +37,125 @@ var ball = {
   yVel: BALL_SPEED
 };
 
-var p1MovingUp = false;
-var p1MovingDown = false;
-
-var p2MovingUp = false;
-var p2MovingDown = false;
-
 var gameOver = false;
-
 var livesRemaining = 3;
 var score = 0;
 
 function keyDown(event) {
   switch (event.keyCode) {
-
-  case Q_KEY:
-    p1MovingUp = true;
-    break;
-
-  case A_KEY:
-    p1MovingDown = true;
-    break;
-
-  case P_KEY:
-    p2MovingUp = true;
-    break;
-
-  case L_KEY:
-    p2MovingDown = true;
-    break;
+  case Q_KEY: leftPaddle.movingUp = true;    break;
+  case A_KEY: leftPaddle.movingDown = true;  break;
+  case P_KEY: rightPaddle.movingUp = true;   break;
+  case L_KEY: rightPaddle.movingDown = true; break;
   }
 }
 
 function keyUp(event) {
   switch (event.keyCode) {
+  case Q_KEY: leftPaddle.movingUp = false;    break;
+  case A_KEY: leftPaddle.movingDown = false;  break;
+  case P_KEY: rightPaddle.movingUp = false;   break;
+  case L_KEY: rightPaddle.movingDown = false; break;
+  }
+}
 
-  case Q_KEY:
-    p1MovingUp = false;
-    break;
+function updatePaddle(paddle) {
+  var paddleVel = 0;
 
-  case A_KEY:
-    p1MovingDown = false;
-    break;
+  if (paddle.movingUp) {
+    paddleVel -= PADDLE_SPEED;
+  }
 
-  case P_KEY:
-    p2MovingUp = false;
-    break;
+  if (paddle.movingDown) {
+    paddleVel += PADDLE_SPEED;
+  }
 
-  case L_KEY:
-    p2MovingDown = false;
-    break;
+  paddle.y += paddleVel;
+
+  if (paddle.y + PADDLE_HEIGHT > CANVAS_HEIGHT) {
+    paddle.y = CANVAS_HEIGHT - PADDLE_HEIGHT;
+  }
+
+  if (paddle.y < 0) {
+    paddle.y = 0;
+  }
+}
+
+function updateBall(ball) {
+  ball.x += ball.xVel;
+  ball.y += ball.yVel;
+
+  if (ball.x - BALL_RADIUS > CANVAS_WIDTH || ball.x + BALL_RADIUS < 0) {
+
+    ball.x = CANVAS_WIDTH / 2;
+    ball.y = CANVAS_HEIGHT / 2;
+    livesRemaining--;
+
+  } else if ((ball.xVel > 0 &&
+         ball.x + BALL_RADIUS > rightPaddle.x &&
+         ball.x - BALL_RADIUS <= rightPaddle.x &&
+         ball.y + BALL_RADIUS >= rightPaddle.y &&
+         ball.y - BALL_RADIUS <= rightPaddle.y + PADDLE_HEIGHT) ||
+        (ball.x - BALL_RADIUS < leftPaddle.x + PADDLE_WIDTH &&
+         ball.x + BALL_RADIUS >= leftPaddle.x + PADDLE_WIDTH &&
+         ball.y + BALL_RADIUS >= leftPaddle.y &&
+         ball.y - BALL_RADIUS <= leftPaddle.y + PADDLE_HEIGHT)) {
+
+    ball.xVel = -ball.xVel;
+    score++;
+  }
+
+  if ((ball.yVel > 0 && ball.y + BALL_RADIUS > CANVAS_HEIGHT) ||
+      (ball.yVel < 0 && ball.y - BALL_RADIUS < 0)) {
+
+    ball.yVel = -ball.yVel;
+  }
+
+  if (livesRemaining < 0) {
+    gameOver = true;
   }
 }
 
 function tick() {
-
   if (!gameOver) {
-    var p1PaddleVel = 0;
-
-    if (p1MovingUp) {
-      p1PaddleVel -= PADDLE_SPEED;
-    }
-
-    if (p1MovingDown) {
-      p1PaddleVel += PADDLE_SPEED;
-    }
-
-    p1Paddle.y += p1PaddleVel;
-
-    if (p1Paddle.y + PADDLE_HEIGHT > CANVAS_HEIGHT) {
-      p1Paddle.y = CANVAS_HEIGHT - PADDLE_HEIGHT;
-    }
-
-    if (p1Paddle.y < 0) {
-      p1Paddle.y = 0;
-    }
-
-    var p2PaddleVel = 0;
-
-    if (p2MovingUp) {
-      p2PaddleVel -= PADDLE_SPEED;
-    }
-
-    if (p2MovingDown) {
-      p2PaddleVel += PADDLE_SPEED;
-    }
-
-    p2Paddle.y += p2PaddleVel;
-
-    if (p2Paddle.y + PADDLE_HEIGHT > CANVAS_HEIGHT) {
-      p2Paddle.y = CANVAS_HEIGHT - PADDLE_HEIGHT;
-    }
-
-    if (p2Paddle.y < 0) {
-      p2Paddle.y = 0;
-    }
-
-    ball.x += ball.xVel;
-    ball.y += ball.yVel;
-
-    if (ball.xVel > 0) {
-      if (ball.x - BALL_RADIUS > CANVAS_WIDTH) {
-        ball.x = CANVAS_WIDTH / 2;
-        ball.y = CANVAS_HEIGHT / 2;
-
-        livesRemaining--;
-
-      } else if (ball.x + BALL_RADIUS > p2Paddle.x &&
-                 ball.x - BALL_RADIUS <= p2Paddle.x &&
-                 ball.y + BALL_RADIUS >= p2Paddle.y &&
-                 ball.y - BALL_RADIUS <= p2Paddle.y + PADDLE_HEIGHT) {
-
-        ball.xVel = -ball.xVel;
-        score += 1;
-      }
-
-    } else {
-      if (ball.x + BALL_RADIUS < 0) {
-        ball.x = CANVAS_WIDTH / 2;
-        ball.y = CANVAS_HEIGHT / 2;
-
-        livesRemaining--;
-
-      } else if (ball.x - BALL_RADIUS < p1Paddle.x + PADDLE_WIDTH &&
-                 ball.x + BALL_RADIUS >= p1Paddle.x + PADDLE_WIDTH &&
-                 ball.y + BALL_RADIUS >= p1Paddle.y &&
-                 ball.y - BALL_RADIUS <= p1Paddle.y + PADDLE_HEIGHT) {
-
-        ball.xVel = -ball.xVel;
-        score += 1;
-      }
-    }
-
-    if (ball.yVel > 0) {
-      if (ball.y + BALL_RADIUS > CANVAS_HEIGHT) {
-        ball.yVel = -ball.yVel;
-      }
-
-    } else {
-      if (ball.y - BALL_RADIUS < 0) {
-        ball.yVel = -ball.yVel;
-      }
-    }
-
-    if (livesRemaining < 0) {
-      gameOver = true;
-    }
+    updatePaddle(leftPaddle);
+    updatePaddle(rightPaddle);
+    updateBall(ball);
   }
 }
 
+function drawRect(ctx, x, y, w, h, color) {
+  ctx.strokeStyle = color;
+  ctx.fillStyle = color;
+
+  ctx.fillRect(x, y, w, h);
+}
+
+function drawCircle(ctx, x, y, radius, color) {
+  ctx.strokeStyle = color;
+  ctx.fillStyle = color;
+
+  ctx.beginPath();
+  ctx.arc(x, y, radius, 0, Math.PI * 2, false);
+  ctx.fill();
+}
+
 function draw() {
-  context.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  var color = 'white';
 
-  context.strokeStyle = 'white';
-  context.fillStyle = 'white';
+  drawRect(ctx, leftPaddle.x, leftPaddle.y, PADDLE_WIDTH, PADDLE_HEIGHT, color);
+  drawRect(ctx, rightPaddle.x, rightPaddle.y, PADDLE_WIDTH, PADDLE_HEIGHT, color);
+  drawCircle(ctx, ball.x, ball.y, BALL_RADIUS, 'white');
 
-  context.fillRect(p1Paddle.x, p1Paddle.y, PADDLE_WIDTH, PADDLE_HEIGHT);
-  context.fillRect(p2Paddle.x, p2Paddle.y, PADDLE_WIDTH, PADDLE_HEIGHT);
+  ctx.font = '24px monospace';
+  ctx.fillText("lives: " + livesRemaining, 0, 24);
 
-  context.beginPath();
-  context.arc(ball.x, ball.y, BALL_RADIUS, 0, Math.PI * 2, false);
-  context.fill();
-
-  context.font = '24px monospace';
-  context.fillText("lives: " + livesRemaining, 0, 24);
-
-  context.font = '24px monospace';
-  context.fillText("score: " + score, CANVAS_WIDTH - 200, 24);
+  ctx.font = '24px monospace';
+  ctx.fillText("score: " + score, CANVAS_WIDTH - 200, 24);
 
   if (gameOver) {
-    context.fillText("GAME OVER", CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2);
+    ctx.fillText("GAME OVER", CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2);
   }
 }
 
@@ -216,12 +168,13 @@ function loop(time) {
   });
 }
 
-function start() {
+function run() {
+  window.onkeydown = keyDown;
+  window.onkeyup = keyUp;
+
   window.requestAnimationFrame(function(time) {
     loop(time);
   });
 }
 
-window.onkeydown = keyDown;
-window.onkeyup = keyUp;
-start();
+run();
