3967f4ee4efaff3774b74f7a636140f2def4ba36
Adam Sheehan <a.t.sheehan@gmail.com>
2014-03-06 09:54:38 -0500

    Shape attaches itself to grid after collision.

--- public/js/tetris/game.js
+++ public/js/tetris/game.js
@@ -16,6 +16,23 @@ var GRID_HEIGHT = BLOCK_SIZE * ROWS;
 var GRID_X = (CANVAS_WIDTH - GRID_WIDTH) / 2;
 var GRID_Y = (CANVAS_HEIGHT - GRID_HEIGHT) / 2;
 
+function generateRandomShape() {
+  var shapeIndex = getRandomInt(0, SHAPES.length - 1);
+  var rotationIndex = 0;
+  var blocks = SHAPES[shapeIndex][rotationIndex];
+
+  var x = (COLUMNS - blocks.length) / 2;
+  var y = 0;
+
+  return {
+    x: x,
+    y: y,
+    blocks: blocks,
+    rotation: rotationIndex,
+    index: shapeIndex
+  };
+}
+
 function moveShapeLeft(game) {
   game.currentShape.x--;
 }
@@ -24,8 +41,83 @@ function moveShapeRight(game) {
   game.currentShape.x++;
 }
 
+function attachShapeToGrid(shape, grid) {
+  for (var row = 0; row < shape.blocks.length; row++) {
+    for (var col = 0; col < shape.blocks[row].length; col++) {
+
+      if (shape.blocks[row][col] !== 0) {
+        grid[shape.y + row][shape.x + col] = shape.blocks[row][col];
+      }
+    }
+  }
+}
+
+function hasCollided(shape, grid) {
+  for (var row = 0; row < shape.blocks.length; row++) {
+    for (var col = 0; col < shape.blocks[row].length; col++) {
+      if (shape.blocks[row][col] !== 0) {
+        if (shape.y + row >= ROWS ||
+            shape.x + col > COLUMNS ||
+            shape.x + col < 0 ||
+            grid[shape.y + row][shape.x + col] !== 0) {
+
+          return true;
+        }
+      }
+    }
+  }
+
+  return false;
+}
+
+function clearRow(grid, rowIndexToClear) {
+  for (var row = rowIndexToClear; row > 0; row--) {
+    grid[row] = grid[row - 1];
+  }
+
+  grid[0] = []
+  for (var col = 0; col < COLUMNS; col++) {
+    grid[0][col] = 0;
+  }
+}
+
+function checkForFullRows(grid) {
+  for (var rowIndex = 1; rowIndex < grid.length; rowIndex++) {
+    var row = grid[rowIndex];
+    var emptyCellFound = false;
+
+    for (var col = 0; col < row.length; col++) {
+      if (row[col] == 0) {
+        emptyCellFound = true;
+        break;
+      }
+    }
+
+    if (!emptyCellFound) {
+      clearRow(grid, rowIndex);
+    }
+  }
+}
+
 function moveShapeDown(game) {
   game.currentShape.y++;
+  if (hasCollided(game.currentShape, game.grid)) {
+    game.currentShape.y--;
+
+    attachShapeToGrid(game.currentShape, game.grid);
+    checkForFullRows(game.grid);
+    game.currentShape = generateRandomShape();
+  }
+}
+
+function rotateShape(game) {
+  var shape = game.currentShape;
+
+  shape.rotation++;
+
+  if (shape.rotation >= 4) {
+    shape.rotation = 0;
+  }
 }
 
 function keyDown(game, event) {
@@ -35,6 +127,7 @@ function keyDown(game, event) {
   case LEFT_KEY: moveShapeLeft(game); break;
   case RIGHT_KEY: moveShapeRight(game); break;
   case DOWN_KEY: moveShapeDown(game); break;
+  case UP_KEY: rotateShape(game); break;
   default: handled = false; break;
   }
 
@@ -44,6 +137,12 @@ function keyDown(game, event) {
 }
 
 function tick(game) {
+  game.dropCounter--;
+
+  if (game.dropCounter < 0) {
+    moveShapeDown(game);
+    game.dropCounter = 60;
+  }
 }
 
 function draw(game) {
@@ -53,17 +152,16 @@ function draw(game) {
 
   if (game.currentShape !== null) {
     var shape = game.currentShape;
-    var blocks = SHAPES[shape.index][shape.rotation];
 
-    for (var i = 0; i < blocks.length; i++) {
-      if (blocks[i] !== 0) {
-        var row = shape.y + Math.floor(i / SHAPE_LENGTH);
-        var col = shape.x + (i % SHAPE_LENGTH);
+    for (var row = 0; row < shape.blocks.length; row++) {
+      for (var col = 0; col < shape.blocks[row].length; col++) {
+        if (shape.blocks[row][col] !== 0) {
 
-        var x = GRID_X + (col * BLOCK_SIZE);
-        var y = GRID_Y + (row * BLOCK_SIZE);
+          var x = GRID_X + ((shape.x + col) * BLOCK_SIZE);
+          var y = GRID_Y + ((shape.y + row) * BLOCK_SIZE);
 
-        drawRect(context, x, y, BLOCK_SIZE, BLOCK_SIZE, 'blue');
+          drawRect(context, x, y, BLOCK_SIZE, BLOCK_SIZE, 'blue');
+        }
       }
     }
   }
@@ -106,7 +204,8 @@ function generateGrid() {
 function run() {
   var game = {
     grid: generateGrid(),
-    currentShape: { x: 0, y: 0, index: 0, rotation: 0 }
+    currentShape: generateRandomShape(),
+    dropCounter: 60
   };
 
   window.onkeydown = function(event) {
